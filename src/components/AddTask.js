1fd479ec9174b4610b4941214bbe08d6b196ea94
Paulina Słabosz <sejmipool@gmail.com>
2023-03-07 18:03:20 +0100

    Added basic styles for whole project + styles for AddTask component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,16 +1,22 @@
 import React from 'react';
-
+import './AddTask.css';
 const AddTask = () => {
   return (
     <form>
-      <input type='text' placeholder='dodaj zadanie' />
-      <label htmlFor='checkbox'>
-        <input type='checkbox' id='checkbox' />
-        Priorytet
-      </label>
+      <div className='inlineWrapper'>
+        <label htmlFor='addTask'>
+          <input type='text' placeholder='dodaj zadanie' id='addTask' />
+        </label>
+
+        <label htmlFor='checkbox' className='priority'>
+          <input type='checkbox' id='checkbox' />
+          Priorytet
+        </label>
+      </div>
       <label htmlFor='date'>
         Do kiedy zrobić: <input type='date' id='date' />
       </label>
+
       <button>Dodaj</button>
     </form>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <>
-        <AddTask />
+        <div className='wrapper'>
+          <AddTask />
+        </div>
       </>
     );
   }
