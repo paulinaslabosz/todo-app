8f23e632235414d0bbe1fee6da8a2b979c96aa75
Paulina Słabosz <sejmipool@gmail.com>
2023-03-07 16:55:39 +0100

    Created structure for 'add task'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import './App.css';
 class App extends Component {
   state = {};
   render() {
-    return <></>;
+    return (
+      <>
+        <div>
+          <form>
+            <input type='text' placeholder='dodaj zadanie' />
+            <label htmlFor='checkbox'>
+              <input type='checkbox' id='checkbox' />
+              Priorytet
+            </label>
+            <label htmlFor='date'>
+              Do kiedy zrobić: <input type='date' id='date' />
+            </label>
+            <button>Dodaj</button>
+          </form>
+        </div>
+      </>
+    );
   }
 }
 
