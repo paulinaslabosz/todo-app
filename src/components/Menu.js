73aa22ba614e0832f49101e104f28f0e69fa1cd7
Paulina Słabosz <sejmipool@gmail.com>
2023-05-30 18:38:50 +0200

    Hamburger Menu: create component and basic elements for media queries

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Menu.css';
+
+function Menu() {
+  return (
+    <ul className='menu_list'>
+      <li>All</li>
+      <li>Priorities</li>
+      <li>Non-priorities</li>
+    </ul>
+  );
+}
+
+export default Menu;
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Task from './Task';
 import DoneTask from './DoneTask';
+import Menu from './Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import './TasksList.css';
 
 const TasksList = (props) => {
+  const [isActive, setActive] = useState(false);
+
   const doneTasks = props.tasks
     .filter((task) => task.active === false)
     .map((task) => (
@@ -19,6 +24,7 @@ const TasksList = (props) => {
         doneDate={task.doneDate}
       />
     ));
+
   const activeTasks = props.tasks
     .filter((task) => task.active === true)
     .sort((a, b) => a.title.localeCompare(b.title))
@@ -37,7 +43,26 @@ const TasksList = (props) => {
 
   return (
     <div className='todo_list'>
+      {isActive ? (
+        <>
+          <FontAwesomeIcon
+            onClick={() => setActive(!isActive)}
+            icon={faXmark}
+            className='todo_menu'
+          />
+          <Menu />
+        </>
+      ) : (
+        <FontAwesomeIcon
+          onClick={() => setActive(!isActive)}
+          icon={faBars}
+          size='lg'
+          className='todo_menu'
+        />
+      )}
+
       <h2 className='todo_header'>Tasks to do:</h2>
+
       <ul>{activeTasks.length !== 0 ? activeTasks : <p>No tasks</p>}</ul>
       {doneTasks.length !== 0 ? (
         <>
