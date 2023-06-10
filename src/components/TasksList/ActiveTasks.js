c568c96257a45f934e900143cbfe9533c8e587eb
Paulina Słabosz <sejmipool@gmail.com>
2023-06-10 19:16:25 +0200

    Created array wuth priority tasks

--- src/components/TasksList/ActiveTasks.js
+++ src/components/TasksList/ActiveTasks.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Task from '../SingleTask/Task';
+import { Context } from '../../context';
 
 function ActiveTasks(props) {
-  return <ul>{props.tasks}</ul>;
+  const context = useContext(Context);
+
+  const priorityTasks = context
+    .filter((task) => task.active === true && task.priority === true)
+    .sort((a, b) => a.title.localeCompare(b.title))
+    .map((task) => (
+      <Task
+        key={task.id}
+        id={task.id}
+        title={task.title}
+        date={task.date}
+        priority={task.priority}
+        active={task.active}
+        changeStatus={props.changeStatus}
+        deleteTask={props.deleteTask}
+      />
+    ));
+
+  return (
+    <>
+      <ul>{props.tasks}</ul>
+      <ul>{priorityTasks}</ul>
+    </>
+  );
 }
 
 export default ActiveTasks;
